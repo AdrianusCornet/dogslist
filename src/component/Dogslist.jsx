3dd32cf102b4502e6d5258ca10f49314c834d9f6
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-13 12:22:19 +0200

    make dogBreads render

--- src/component/Dogslist.jsx
+++ src/component/Dogslist.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
 export default class Dogslist extends Component {
+  state = {
+    dogBreeds: 'potato',
+  }
+  
+  renderDogBreads() {
+    return this.state.dogBreeds === null && 'loading...'
+  }
   render() {
     return (
       <div className='dogs-list'>
         <h1>Dogs List</h1>
+
+        { this.state.dogBreeds === null && 'loading...' }
       </div>
     )
   }
