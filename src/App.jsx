3bc8ebc7c070c955e5347f99c5b1a054b16b9dba
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-13 12:01:09 +0200

    link and call Dogslist

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Dogslist from './component/Dogslist'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        
+        < Dogslist />
       </div>
     );
   }
