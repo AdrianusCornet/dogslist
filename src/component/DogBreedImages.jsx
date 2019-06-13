4f738a1da310a3032c413e84b2638b4cafb8d128
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-13 18:14:23 +0200

    images can render

--- src/component/DogBreedImages.jsx
+++ src/component/DogBreedImages.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-export default function DogBreedImages(props) {
-  return ( 
-    <div className="dog-breed-images">
-      <h1>Dogs Breed images</h1>
+export default class DogBreedImages extends Component {
+  renderImages() {
+    if (this.props.images === null) {
+      return 'loading...'
+    }
+    return(
+      this.props.images.map(image => {
+        return (
+          <img src={image} alt='dog' key={image} />
+        )
+      })
+    )
+  }
+  render() {
+    return (
+      <div className="dog-breed-images">
+        <h1>Dogs Breed images</h1>
 
-      This page will show images of the { props.breed } breed.
+        This page will show images of the {(this.props.breed)} breed.
 
       <Link to="/">Go back to the index</Link>
-    </div>
-  )
+      <br/>
+      {this.renderImages()}
+      </div>
+    )
+  }
 }
--- src/component/DogBreedImagesContener.jsx
+++ src/component/DogBreedImagesContener.jsx
@@ -5,17 +5,25 @@ export default class DogBreedImagesContener extends Component {
   state = {
     images: null
   }
+  updateImages(images) {
+    this.setState({
+      images: images
+    })
+  }
 
   componentDidMount() {
     const breed = this.props.match.params.breed
     fetch(`https://dog.ceo/api/breed/${encodeURIComponent(breed)}/images`)
       .then(response => response.json())
+      .then(myJson => this.updateImages(myJson.message))
       .catch(console.error)
   }
 
   render() {
     return (
-      < DogBreedImages breed ={this.props.match.params.breed} />
+      < DogBreedImages
+        breed ={this.props.match.params.breed}
+        images={this.state.images} />
     )
   }
 }
