99a0b06046b53bd5eaf587a9bcec82b08f465d7c
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-13 12:04:34 +0200

    add h1

--- src/component/Dogslist.jsx
+++ src/component/Dogslist.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class Dogslist extends Component {
   render() {
     return (
-      <div>
-        
+      <div className='dogs-list'>
+        <h1>Dogs List</h1>
       </div>
     )
   }
