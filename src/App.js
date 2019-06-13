811357a41f79690f57d49e931f06baa6230ca50f
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-13 16:55:47 +0200

    make DBImages avadibale

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import DogsListContainer from './component/DogsListContainer'
 import { Route } from 'react-router-dom'
+import DogBreedImages from './component/DogBreedImages';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Route exact path="/" component={DogsListContainer} />
+        <Route path='/dog-breeds/:breed' component={DogBreedImages} />
       </div>
     );
   }
--- src/component/DogBreedImages.jsx
+++ src/component/DogBreedImages.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function DogBreedImages(props) {
-  return (
+  return ( 
     <div className="dog-breed-images">
       <h1>Dogs Breed images</h1>
 
       This page will show images of a specific dog breed.
 
+      <br/>
       <Link to="/">Go back to the index</Link>
     </div>
   )
